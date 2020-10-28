8b97235dc58bdf4becbeac6ede7bed3a873fbe3b
Rajabb4685.github.io <73593020+Rajabb4685@users.noreply.github.com>
2020-10-28 08:44:27 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Rajab Begim"
+let userName = "rajabb4685";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
